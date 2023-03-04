5aad3011d2ee49d543569e81c78c437ccf8f1c69
Eman20Rules <75296642+Eman20Rules@users.noreply.github.com>
2023-03-03 19:29:09 -0600

    Update App.js
    Basic AppBar test

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { AppBar, HStack, IconButton } from "@react-native-material/core";
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! testing this still works</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppBar
+    title="Title"
+    leading={props => (
+      <IconButton icon={props => <Icon name="menu" {...props} />} {...props} />
+    )}
+    trailing={props => (
+      <HStack>
+        <IconButton
+          icon={props => <Icon name="magnify" {...props} />}
+          {...props}
+        />
+        <IconButton
+          icon={props => <Icon name="dots-vertical" {...props} />}
+          {...props}
+        />
+      </HStack>
+    )}
+  />
   );
 }
 
