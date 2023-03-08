c2e7908bef79e5337d0beed1a278af36a782b00b
crschmidt2 <115666285+crschmidt2@users.noreply.github.com>
2023-03-07 19:56:22 -0600

    Added stylesheet import in App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { AppBar, HStack, IconButton } from "@react-native-material/core";
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+import { StyleSheet} from 'react-native';
 
 export default function App() {
   return (
