f56333127d2390485bfefdd6e988f3719a39b6e7
Eman20Rules <75296642+Eman20Rules@users.noreply.github.com>
2023-03-07 23:18:26 -0600

    Update CustomNavigationBar.js

--- components/CustomNavigationBar.js
+++ components/CustomNavigationBar.js
@@ -2,31 +2,6 @@ import { StyleSheet, Text } from "react-native";
 import { Appbar, Menu, Provider } from "react-native-paper";
 import { useState } from "react";
 
-// function CustomNavigationBar({ navigation, back }) {
-//   const [visible, setVisible] = React.useState(false);
-//   const openMenu = () => setVisible(true);
-//   const closeMenu = () => setVisible(false);
-
-//   return (
-//     <Appbar.Header>
-//       {back ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
-//       <Appbar.Content title="My awesome app" />
-//       {!back ? (
-//         <Menu
-//           visible={visible}
-//           onDismiss={closeMenu}
-//           anchor={
-//             <Appbar.Action icon="menu" color="white" onPress={openMenu} />
-//           }>
-//           <Menu.Item onPress={() => {console.log('Option 1 was pressed')}} title="Option 1" />
-//           <Menu.Item onPress={() => {console.log('Option 2 was pressed')}} title="Option 2" />
-//           <Menu.Item onPress={() => {console.log('Option 3 was pressed')}} title="Option 3" disabled />
-//         </Menu>
-//       ) : null}
-//     </Appbar.Header>
-//   );
-// }
-
 function CustomNavigationBar({ navigation }) {
   const [visible, setVisible] = useState(false);
 
